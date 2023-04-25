45b412407b2890c80a1b4273de4b9facef24ecd5
mrmatteorusso <mrmatteorusso@gmail.com>
2023-04-25 12:33:45 +0200

    adding google tag manager code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,10 +2,19 @@ import React from "react";
 
 function Home() {
   return (
-    <div className="main-title">
-      <h3>Matteo Russo</h3>
-      <p>Web developer</p>
-    </div>
+    <html>
+      <body>
+        <head>
+          <meta name="description" content="Explore my latest web development project and see how I bring innovative designs to life with seamless functionality. From custom coding to user-friendly interfaces, I leverage cutting-edge technologies to build responsive and dynamic websites that engage users and drive results. Check out my portfolio and discover the power of web development today." />
+          <title>Matteo Russo - Frontend Developer</title>
+        </head>
+
+        <div className="main-title">
+          <h3>Matteo Russo</h3>
+          <p>Frontend Developer</p>
+        </div>
+      </body>
+    </html>
   );
 }
 
